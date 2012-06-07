079a28a9c5728934eaafe6a6f3f9fd6c7fd2790d
Luu Gia Thuy <luugiathuy@gmail.com>
2012-06-07 21:30:53 +0800

    Initial Commit

---
+++ PhotoAlbum/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  PhotoAlbum
+//
+//  Created by Gia Thuy Luu on 7/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
